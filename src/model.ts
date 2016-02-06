d678707a7c5d75dc98fa03bdac40a1c57ec3a7aa
bouzuya <m@bouzuya.net>
2016-02-06 15:17:49 +0900

    change action interface

--- src/model.ts
+++ src/model.ts
@@ -5,6 +5,8 @@ import { routes } from './routes';
 import { view } from './view';
 import { Property, Updater } from './property';
 
+// Resource
+
 // dummy storage
 const users: User[] = [
   { id: 0, name: 'user1', bio: 'Hello!' },
@@ -20,15 +22,39 @@ const fetchUser = (id: number): Promise<User> => {
   return Promise.resolve(users[id]);
 };
 
-const listUserAction = (): Promise<State> => {
-  return fetchUsers()
-    .then(users => ({ users, user: null }));
+// Updater
+
+const listUserAction = (): Updater<State> => {
+  return (state: State): Promise<State> => {
+    return fetchUsers()
+      .then(user => Object.assign({}, state, ({ users, user: null })));
+  };;
 };
 
-const showUserAction = ([id]: string[]): Promise<State> => {
-  const userId = parseInt(id, 10);
-  return fetchUser(userId)
-    .then(user => ({ users: [], user }));
+const showUserAction = (id: string): Updater<State> => {
+  return (state: State): Promise<State> => {
+    const userId = parseInt(id, 10);
+    return fetchUser(userId)
+      .then(user => Object.assign({}, state, ({ users: [], user })));
+  };
+};
+
+const makeChangeName = (): Updater<State> => {
+  return (state: State): Promise<State> => {
+    const { user } = state;
+    if (!user) return Promise.resolve(state);
+    const { name } = user;
+    const newUser = Object.assign({}, user, { name: name + '!' });
+    const newState = Object.assign({}, state, { user: newUser });
+    return Promise.resolve(newState);
+  };
+};
+
+// init
+
+type InitResponse = {
+  emit: (eventName: string, options?: any) => void;
+  on: (eventName: string, options?: any) => void;
 };
 
 type RequestActionOptions = {
@@ -36,43 +62,33 @@ type RequestActionOptions = {
   done: (error: Error, vtree?: any) => void;
 };
 
-const requestAction = ({ path, done }: RequestActionOptions): void => {
-  routes([
-    ['/users', listUserAction],
-    ['/users/:id', showUserAction]
-  ], path)
-    .then((state: State) => {
-      const vtree = view(state, true);
-      done(null, vtree);
-    }, (error: Error) => {
-      done(error);
-    });
-};
-
-const changeName = (state: State): Promise<State> => {
-  const { user } = state;
-  if (!user) return Promise.resolve(state);
-  const { name } = user;
-  const newUser = Object.assign({}, user, { name: name + '!' });
-  const newState = Object.assign({}, state, { user: newUser });
-  return Promise.resolve(newState);
-};
-
-type InitResponse = {
-  emit: (eventName: string, options?: any) => void;
-  on: (eventName: string, options?: any) => void;
-};
-
 const init = (state?: any): InitResponse => {
   const property = new Property(state);
   const events = new EventEmitter();
-  events.on('request', requestAction);
-  events.on('change-name', () => events.emit('update', changeName));
+  events.on('request', ({ path, done }: RequestActionOptions): void => {
+    const updater = routes([
+      ['/users', () => listUserAction()],
+      ['/users/:id', ([id]: string[]) => showUserAction(id)]
+    ], path);
+    property
+      .update(updater)
+      .then(state => {
+        const vtree = view(state, true);
+        done(null, vtree);
+      }, (error: Error) => {
+        done(error);
+      });
+  });
+  events.on('change-name', () => events.emit('update', makeChangeName()));
+  events.on('list-users', () => events.emit('update', listUserAction()));
+  events.on('show-user', (id: string) =>
+    events.emit('update', showUserAction(id))
+  );
   events.on('update', (update: Updater<State>): void => {
     property
       .update(update)
-      .then(newState => {
-        const vtree = view(newState, false);
+      .then(state => {
+        const vtree = view(state, false);
         events.emit('vtree-updated', vtree);
       });
   });
