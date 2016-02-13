eefd6c7ee8611f4951f0744790d5ac15110d7a4e
bouzuya <m@bouzuya.net>
2016-02-14 05:34:15 +0900

    separate HistoryRouter from Router

--- src/libs/router.ts
+++ src/libs/router.ts
@@ -4,41 +4,14 @@ type Route<T> = [string, (params: string[]) => T];
 
 class Router<T> {
   private config: Route<T>[];
-  private window: any;
-  private history: History;
 
   constructor(config: Route<T>[]) {
-    this.window = Function('return this')();
-    this.history = this.window.history;
     this.config = config;
   }
 
-  back(): void {
-    if (this.history) {
-      this.history.back();
-    }
-  }
-
-  go(path: string, replace: boolean = false): T {
-    if (this.history) {
-      const f = replace ? history.replaceState : history.pushState;
-      f.apply(history, [null, null, path]);
-    }
-    return this.routes(this.config, path);
-  }
-
-  start(): void {
-    if (this.history) {
-      this.window.addEventListener('popstate', () => {
-        const path = this.window.location.pathname;
-        return this.routes(this.config, path);
-      }, false);
-    }
-  }
-
-  private routes(routes: Route<T>[], path: string): T {
-    for (var i = 0; i < routes.length; i++) {
-      const route = routes[i];
+  route(path: string): T {
+    for (var i = 0; i < this.config.length; i++) {
+      const route = this.config[i];
       const [pathPattern, handler] = route;
       const match = pathToRegexp(pathPattern).exec(path);
       if (match) {
