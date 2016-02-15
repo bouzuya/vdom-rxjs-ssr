9080a69b04a49ea010d0197a7573a1351c2de941
bouzuya <m@bouzuya.net>
2016-02-16 00:21:57 +0900

    fix initializer name

--- src/app/initializers/show-user.ts
+++ src/app/initializers/show-user.ts
@@ -2,7 +2,7 @@ import { InitializerParameters } from '../../framework/initializer';
 import { State } from '../models/state';
 import { User } from '../models/user';
 
-export default function showUserAction(
+export default function showUserInitializer(
   params: InitializerParameters
 ): Promise<State> {
   const userId = parseInt(params['id'], 10);
