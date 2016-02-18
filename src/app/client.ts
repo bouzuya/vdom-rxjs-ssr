4f159cf13f22cdbc71ef32489d14dbe2d8f04045
bouzuya <m@bouzuya.net>
2016-02-19 08:16:16 +0900

    refactor

--- src/app/client.ts
+++ src/app/client.ts
@@ -6,27 +6,15 @@ import { State } from './models/state';
 import { User } from './models/user';
 import { view } from './view';
 
-const app = (
-  { state, dom }: { state: State, dom: DOM }
-): Observable<State> => {
-  const timer$ = Observable
-    .interval(1000);
-  const click$ = dom
-    .on('button', 'click')
-    .map((event) => (<any> event.target).dataset.userId);
-  const users$ = Observable
-    .of(state.users)
-    .merge(click$.map((id) => (users: User[]) => {
-      const user = users.filter(user => user.id === parseInt(id, 10))[0];
-      // FIXME:
-      if (user) user.likeCount += 1;
-      return users;
-    }))
-    .scan((users: User[], update: (users: User[]) => User[]) => update(users));
-  const user$ = Observable
-    .of(state.user)
+const user$ = (
+  state: User,
+  clickedUserId$: Observable<string>,
+  timer$: Observable<any>
+): Observable<User> => {
+  return Observable
+    .of(state)
     .merge(
-      click$.map(() => (user: User) => {
+      clickedUserId$.map(() => (user: User) => {
         // FIXME:
         if (user) user.likeCount += 1;
         return user;
@@ -37,9 +25,35 @@ const app = (
         return user;
       }))
     .scan((user: User, update: (user: User) => User) => update(user));
+};
+
+const users$ = (
+  state: User[],
+  clickedUserId$: Observable<string>
+): Observable<User[]> => {
+  return Observable
+    .of(state)
+    .merge(clickedUserId$.map((id) => (users: User[]) => {
+      const user = users.filter(user => user.id === parseInt(id, 10))[0];
+      // FIXME:
+      if (user) user.likeCount += 1;
+      return users;
+    }))
+    .scan((users: User[], update: (users: User[]) => User[]) => update(users));
+};
+
+const app = (
+  { state, dom }: { state: State, dom: DOM }
+): Observable<State> => {
+  const timer$ = Observable
+    .interval(1000);
+  const clickedUserId$ = dom
+    .on('button', 'click')
+    .map((event) => (<any> event.target).dataset.userId);
   const state$ = Observable
     .combineLatest(
-      users$, user$,
+      users$(state.users, clickedUserId$),
+      user$(state.user, clickedUserId$, timer$),
       (users, user) => {
         return { users, user };
       });
