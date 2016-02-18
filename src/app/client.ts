a16d30dae184e9891f7f24361bbc9a62a28f6d71
bouzuya <m@bouzuya.net>
2016-02-18 23:06:21 +0900

    add click-like event

--- src/app/client.ts
+++ src/app/client.ts
@@ -3,15 +3,24 @@ import { Client } from '../framework/client';
 import { DOM } from '../framework/dom';
 import { VTree } from '../framework/view';
 import { State } from './models/state';
+import { User } from './models/user';
 import { view } from './view';
 
 const app = (
   { state, dom }: { state: State, dom: DOM }
 ): Observable<State> => {
+  const click$ = dom
+    .on('button', 'click');
   const users$ = Observable
     .of(state.users);
   const user$ = Observable
     .of(state.user)
+    .merge(click$.map(() => (user: User) => {
+      // FIXME:
+      user.likeCount += 1;
+      return user;
+    }))
+    .scan((user: User, update: (user: User) => User) => update(user));
   const state$ = Observable
     .combineLatest(
       users$, user$,
