7789b55aa82ad258ead88df6f12d29a873a69b32
bouzuya <m@bouzuya.net>
2016-02-20 11:04:08 +0900

    refactor users

--- src/app/properties/users.ts
+++ src/app/properties/users.ts
@@ -1,38 +1,24 @@
 import { Observable } from 'rxjs';
-import { RouteResult } from '../../framework/router';
 
 import { Action } from '../models/action';
 import { User } from '../models/user';
+import addLikeCount$ from './users/add-like-count';
+import clear$ from './users/clear';
+import reset$ from './users/reset';
 
 export default function users$(
   state: User[],
   action$: Observable<Action>
 ): Observable<User[]> {
-  type Updater = (users: User[]) => User[];
-  const clearUsersUpdater$ = action$
-    .filter(({ type }) => type === 'path-change')
-    .filter(({ params: { route: { name } } }) => name !== 'user#index')
-    .map(() => () => <User[]> []);
-  const resetUsersUpdater$ = action$
-    .filter(({ type }) => type === 'go-to-user-index')
-    .mergeMap(() => Observable.fromPromise(User.fetchUsers()))
-    .map((users) => () => users);
-  const incrementLikeCountUpdater$ = action$
-    .filter(({ type }) => type === 'increment-like-count')
-    .map(({ params: { id } }) => (users: User[]) => {
-      const user = users.filter(user => user.id === id)[0];
-      if (user) user.likeCount += 1; // FIXME
-      return users;
-    });
-  const updater$: Observable<Updater> = Observable
-    .merge(
-      clearUsersUpdater$,
-      incrementLikeCountUpdater$,
-      resetUsersUpdater$
-    );
   return Observable
     .of(state)
-    .merge(updater$)
-    .scan((users: User[], updater: Updater) => updater(users));
+    .merge(
+      addLikeCount$,
+      clear$,
+      reset$
+    )
+    .scan((users: User[], updater: (users: User[]) => User[]) => {
+      return updater(users);
+    });
 };
 
