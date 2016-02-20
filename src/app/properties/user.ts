10f58c00c6a4f01ea5eb2fe6c9722daef58d1760
bouzuya <m@bouzuya.net>
2016-02-20 10:59:12 +0900

    refactor user

--- src/app/properties/user.ts
+++ src/app/properties/user.ts
@@ -1,47 +1,23 @@
 import { Observable } from 'rxjs';
-import { RouteResult } from '../../framework/router';
 
 import { Action } from '../models/action';
 import { User } from '../models/user';
+import addLikeCount$ from './user/add-like-count';
+import changeName$ from './user/change-name';
+import clear$ from './user/clear';
+import reset$ from './user/reset';
 
 export default function user$(
   state: User,
   action$: Observable<Action>
 ): Observable<User> {
-  type Updater = (user: User) => User;
-  const changeNameUpdater$ = action$
-    .filter(({ type }) => type === 'change-name')
-    .map(() => (user: User) => {
-      if (user) user.name += '!'; // FIXME
-      return user;
-    });
-  const clearUserUpdater$ = action$
-    .filter(({ type }) => type === 'path-change')
-    .filter(({ params: { route: { name, params } } }) => {
-      return name !== 'user#show';
-    })
-    .map(() => () => null);
-  const incrementLikeCountUpdater$ = action$
-    .filter(({ type }) => type === 'increment-like-count')
-    .map(({ params: { id } }) => (user: User) => {
-      if (user) user.likeCount += 1; // FIXME
-      return user;
-    });
-  const resetUserUpdater$ = action$
-    .filter(({ type }) => type === 'go-to-user-show')
-    .mergeMap(({ params: { id } }) => {
-      return Observable.fromPromise(User.fetchUser(id));
-    })
-    .map(user => () => user);
-  const updater$: Observable<Updater> = Observable
-    .merge(
-      changeNameUpdater$,
-      clearUserUpdater$,
-      incrementLikeCountUpdater$,
-      resetUserUpdater$
-    );
   return Observable
     .of(state)
-    .merge(updater$)
-    .scan((user: User, updater: Updater) => updater(user));
+    .merge(
+      addLikeCount$(action$),
+      changeName$(action$),
+      clear$(action$),
+      reset$(action$)
+    )
+    .scan((user: User, updater: (user: User) => User) => updater(user));
 };
