0db8517ea755d2a907a69577beaf24f85f8caab8
bouzuya <m@bouzuya.net>
2016-02-20 10:59:25 +0900

    remove old actions

--- src/actions/list-user-action.ts
+++
@@ -1,9 +0,0 @@
-import { State, Updater } from '../models/state';
-import { User } from '../models/user';
-
-export default function listUserAction(): Updater {
-  return (state: State): Promise<State> => {
-    return User.fetchUsers()
-      .then(users => Object.assign({}, state, ({ users, user: null })));
-  };
-};
